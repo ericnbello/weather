0373ed8c3d81f356f03348b6f2a11a44755da2b2
Eric Bello <ebello219@gmail.com>
2023-09-20 18:43:29 -0400

    removed unnecessary csrf_exempt decorators

--- enhanced_weather_app/views.py
+++ enhanced_weather_app/views.py
@@ -23,14 +23,14 @@ default_unit_system = 'imperial'
 default_location = 'Miami, FL, US'
 # default_alert = 'No current alerts'
 
-@csrf_exempt
+# @csrf_exempt
 def default_page(request):
     units = default_unit_system
     location = request.POST.get('location', default_location)
 
     return render(request, 'base.html', call_api(units, location))
 
-@csrf_exempt
+# @csrf_exempt
 def call_api(unit_system, location):
     try:
         r_1 = httpx.get('http://api.openweathermap.org/geo/1.0/direct?q={0}&limit=5&appid={1}'.format(location, api_key)) 
