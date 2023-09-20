7bbf0c3a4244ca170e72f2920dd0c08c05842768
Eric Bello <ebello219@gmail.com>
2023-09-20 16:54:28 -0400

    removed unused components

--- enhanced_weather_app/settings.py
+++ enhanced_weather_app/settings.py
@@ -22,11 +22,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'process.env.DB_SECRET_KEY'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-# ALLOWED_HOSTS = ['127.0.0.1', '100.26.133.89', 'ec2-100-26-133-89.compute-1.amazonaws.com']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '100.26.133.89', 'd3lgvgrrop0yu.cloudfront.net', 'ec2-100-26-133-89.compute-1.amazonaws.com']
 
-ALLOWED_HOSTS = ['d3lgvgrrop0yu.cloudfront.net']
+# ALLOWED_HOSTS = ['d3lgvgrrop0yu.cloudfront.net']
 
 # Application definition
 
